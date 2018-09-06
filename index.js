7a5780a9d80e4db1af294553d5acf0b2d5ecdd96
Egor <zangedzu333gig@gmail.com>
2018-09-06 18:38:49 +0300

    Create method for create summary script

--- index.js
+++ index.js
@@ -3,3 +3,32 @@ var path = require('path');
 
 const DIR_PATH = process.argv[2];
 const EXTENSION = '.txt';
+
+
+
+function createSummaryScript() {
+    fs.writeFile(
+    	`${DIR_PATH}\\summary.js`,
+    		'const fs = require(\'fs\');\n' +
+    		'const path = require(\'path\');\n' +
+    		'\n' +
+    		'(function getFiles(baseDir) {\n' +
+    		'    fs.readdir(baseDir, function (e, files){\n' +
+    		'        for (let i in files) {\n' +
+    		'            let CURDIR = baseDir + path.sep + files[i];\n' +
+		    '            fs.stat(CURDIR, (e, stats) => {\n' +
+		    '                    if (stats.isDirectory()) {\n' +
+		    '                        getFiles(CURDIR);\n' +
+		    '                    } else {\n' +
+		    '                        console.log(path.relative(__dirname, CURDIR));\n' +
+		    '                    }\n' +
+		    '                }\n' +
+		    '            );\n' +
+		    '        }\n' +
+		    '    });\n' +
+		    '})(__dirname, null);',
+    	(callback) => {
+	        if (callback) { console.log("Error create summary script: " + callback); }
+	    }
+	);
+}
